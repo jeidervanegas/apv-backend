39e64206c8db96c34c48fc05a2871087ab34a18d
jeidervanegas <146299580+jeidervanegas@users.noreply.github.com>
2023-12-21 16:35:50 -0500

    feat:nuevo3  commit

--- helpers/emailRegister.js
+++ helpers/emailRegister.js
@@ -13,20 +13,24 @@ const emailRegister = async(datos) => {
       const { email, name, token } = datos;
 
       //enviar el emial
-
-      const info = await transport.sendMail({
-        from: 'Administrador de Pacientes de Veterinaria',
-        to: email,
-        subject: 'Comprueba tu cuenta en APV',
-        text: 'Comprueba ti cuenta en APV',
-        html: `<p>Hola ${name}, comprueba tu cuenta en APV</p>
-        <p>Tu cuenta está casi lista, solo debes comprobarla dando click en el enlace:
-        <a href="${process.env.FRONTEND_URL}/confirm/${token}">Comprobar cuenta</a> </p>
-        <p>Si tú no creaste esta cuenta puedes ignorar este mensaje</p>
-        `
-      });
-
-      console.log('Mensaje enviado: %s', info.messageId);
+      try {
+        const info = await transport.sendMail({
+          from: 'Administrador de Pacientes de Veterinaria',
+          to: email,
+          subject: 'Comprueba tu cuenta en APV',
+          text: 'Comprueba ti cuenta en APV',
+          html: `<p>Hola ${name}, comprueba tu cuenta en APV</p>
+          <p>Tu cuenta está casi lista, solo debes comprobarla dando click en el enlace:
+          <a href="${process.env.FRONTEND_URL}/confirm/${token}">Comprobar cuenta</a> </p>
+          <p>Si tú no creaste esta cuenta puedes ignorar este mensaje</p>
+          `
+        });
+  
+        console.log('Mensaje enviado: %s', info.messageId);
+      } catch (error) {
+        console.log(error);
+      }
+      
 }
 
 export default emailRegister;
